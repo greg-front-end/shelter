d961d72187e573fc8d5fdd35d5813d1b3792d369
Greg <martinstudyweb@gmail.com>
2022-04-12 00:31:57 +0300

    feat: add dev mode for test

---
+++ public/index.js
@@ -0,0 +1,94 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/pages/main/style.scss":
+/*!***********************************!*\
+  !*** ./src/pages/main/style.scss ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************************!*\
+  !*** ./src/pages/main/script.js ***!
+  \**********************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ "./src/pages/main/style.scss");
+
+// import '../../assets/images/start-screen-puppy.png'
+// import '../../assets/images/about-pets.png'
+// import '../../assets/images/pets-jennifer.png'
+// import '../../assets/images/pets-katrine.png'
+// import '../../assets/images/pets-woody.png'
+// import '../../assets/icons/arrow-left.png'
+// import '../../assets/icons/arrow-right.png'
+// import '../../assets/icons/our_shelter/icon-bowls-and-cups.svg'
+// import '../../assets/icons/our_shelter/icon-collars-leashes.svg'
+// import '../../assets/icons/our_shelter/icon-medicines.svg'
+// import '../../assets/icons/our_shelter/icon-pet-food.svg'
+// import '../../assets/icons/our_shelter/icon-shampoos.svg'
+// import '../../assets/icons/our_shelter/icon-sleeping-area.svg'
+// import '../../assets/icons/our_shelter/icon-toys.svg'
+// import '../../assets/icons/our_shelter/icon-transportation.svg'
+// import '../../assets/icons/our_shelter/icon-vitamins.svg'
+// import '../../assets/images/donation-dog.png'
+// import '../../assets/images/footer-puppy.png'
+// import '../../assets/images/footer-bg.png'
+// import '../../assets/icons/credit-card.svg'
+// import '../../assets/icons/footer/icon-email.svg'
+// import '../../assets/icons/footer/icon-marker.svg'
+// import '../../assets/icons/footer/icon-phone.svg'
+
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
---
+++ public/pets.js
@@ -0,0 +1,135 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/pages/pets/style.scss":
+/*!***********************************!*\
+  !*** ./src/pages/pets/style.scss ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/assets/images/pets/freddie.png":
+/*!********************************************!*\
+  !*** ./src/assets/images/pets/freddie.png ***!
+  \********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "assets/images/freddie..png";
+
+/***/ }),
+
+/***/ "./src/assets/images/pets/pets-charly.png":
+/*!************************************************!*\
+  !*** ./src/assets/images/pets/pets-charly.png ***!
+  \************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "assets/images/pets-charly..png";
+
+/***/ }),
+
+/***/ "./src/assets/images/pets/pets-katrine-2.png":
+/*!***************************************************!*\
+  !*** ./src/assets/images/pets/pets-katrine-2.png ***!
+  \***************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "assets/images/pets-katrine-2..png";
+
+/***/ }),
+
+/***/ "./src/assets/images/pets/pets-scarlet.png":
+/*!*************************************************!*\
+  !*** ./src/assets/images/pets/pets-scarlet.png ***!
+  \*************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "assets/images/pets-scarlet..png";
+
+/***/ }),
+
+/***/ "./src/assets/images/pets/pets-timmy.png":
+/*!***********************************************!*\
+  !*** ./src/assets/images/pets/pets-timmy.png ***!
+  \***********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "assets/images/pets-timmy..png";
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		__webpack_require__.p = "";
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************************!*\
+  !*** ./src/pages/pets/script.js ***!
+  \**********************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ "./src/pages/pets/style.scss");
+/* harmony import */ var _assets_images_pets_freddie_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../assets/images/pets/freddie.png */ "./src/assets/images/pets/freddie.png");
+/* harmony import */ var _assets_images_pets_pets_charly_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../assets/images/pets/pets-charly.png */ "./src/assets/images/pets/pets-charly.png");
+/* harmony import */ var _assets_images_pets_pets_katrine_2_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../assets/images/pets/pets-katrine-2.png */ "./src/assets/images/pets/pets-katrine-2.png");
+/* harmony import */ var _assets_images_pets_pets_scarlet_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../assets/images/pets/pets-scarlet.png */ "./src/assets/images/pets/pets-scarlet.png");
+/* harmony import */ var _assets_images_pets_pets_timmy_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../assets/images/pets/pets-timmy.png */ "./src/assets/images/pets/pets-timmy.png");
+
+
+
+
+
+
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=pets.js.map
